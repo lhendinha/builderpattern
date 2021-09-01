dacffa1cc9dcdfea62cfcde262e055b3f3cced2c
Pedro Henrique Sousa Almeida <pedro.almeida@delage.com.br>
2021-09-01 11:21:36 -0300

    Initial commit

---
+++ src/main/java/Computer.java
@@ -0,0 +1,47 @@
+public class Computer {
+
+    private String SSD;
+    private String RAM;
+
+    private boolean isGraphicsCardEnabled;
+
+    public String getHDD() {
+        return SSD;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public boolean isGraphicsCardEnabled() {
+        return isGraphicsCardEnabled;
+    }
+
+    private Computer(ComputerBuilder builder) {
+        this.SSD = builder.SSD;
+        this.RAM = builder.RAM;
+        this.isGraphicsCardEnabled = builder.isGraphicsCardEnabled;
+    }
+
+    public static class ComputerBuilder{
+
+        private String SSD;
+        private String RAM;
+
+        private boolean isGraphicsCardEnabled;
+
+        public ComputerBuilder(String ssd, String ram){
+            this.SSD = ssd;
+            this.RAM = ram;
+        }
+
+        public ComputerBuilder setGraphicsCardEnabled(boolean isGraphicsCardEnabled) {
+            this.isGraphicsCardEnabled = isGraphicsCardEnabled;
+            return this;
+        }
+
+        public Computer build(){
+            return new Computer(this);
+        }
+    }
+}
